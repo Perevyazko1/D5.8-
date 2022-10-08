2ade152947ed33d0795cbaa5ad27862a12c1e7ec
Perevyazko <a.perevyazko@gmail.com>
2022-10-08 20:01:33 +0300

    настроил вывод полей фильтра

--- news/simpleapp/views.py
+++ news/simpleapp/views.py
@@ -2,28 +2,28 @@
 
 from django.views.generic import ListView, DetailView
 from .models import Product, News
+from  .filters import NewsFilter
 
-
-class ProductsList(ListView):
-    model = Product
-    ordering = 'name'
-    template_name = 'product.html'
-    context_object_name = 'product'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # context['time_now'] = datetime.utcnow()
-        context['next_sale'] = "Распродажа в среду!"
-        return context
-
-
-class ProductDetail(DetailView):
-    # Модель всё та же, но мы хотим получать информацию по отдельному товару
-    model = Product
-    # Используем другой шаблон — product.html
-    template_name = 'product.html'
-    # Название объекта, в котором будет выбранный пользователем продукт
-    context_object_name = 'product'
+# class ProductsList(ListView):
+#     model = Product
+#     ordering = 'name'
+#     template_name = 'product.html'
+#     context_object_name = 'product'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         # context['time_now'] = datetime.utcnow()
+#         context['next_sale'] = "Распродажа в среду!"
+#         return context
+#
+#
+# class ProductDetail(DetailView):
+#     # Модель всё та же, но мы хотим получать информацию по отдельному товару
+#     model = Product
+#     # Используем другой шаблон — product.html
+#     template_name = 'product.html'
+#     # Название объекта, в котором будет выбранный пользователем продукт
+#     context_object_name = 'product'
 
 
 class NewsList(ListView):
@@ -38,6 +38,9 @@ class NewsList(ListView):
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'news'
     paginate_by = 2 # регулируем количество записей на странице
+
+
+
 
 class NewsDetail(DetailView):
     # Модель всё та же, но мы хотим получать информацию по отдельному товару
@@ -54,3 +57,23 @@ class NewsSearch(ListView):
     template_name = 'search.html'
     context_object_name = 'news'
     paginate_by = 10
+
+
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = NewsFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Добавляем в контекст объект фильтрации.
+        context['filterset'] = self.filterset
+        print(context)
+        return context

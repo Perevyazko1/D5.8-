bb4607510919baaccfd2e77f3f420efda458d4c6
Perevyazko <a.perevyazko@gmail.com>
2022-10-15 12:00:16 +0300

    Добавил страницу входа

--- news/news/settings.py
+++ news/news/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'django_filters',
 ]
 SITE_ID = 1
+
+LOGIN_REDIRECT_URL = "/news"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',

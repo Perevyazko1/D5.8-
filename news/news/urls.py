fc1bf388067c46c47cb6a83f2e46aeb575b73715
Perevyazko <a.perevyazko@gmail.com>
2022-10-15 12:12:08 +0300

    Добавил страницу регистрации

--- news/news/urls.py
+++ news/news/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('accounts/', include('django.contrib.auth.urls')),
+   path("accounts/", include("accounts.urls")),
    path('news/', include('simpleapp.urls')),
    # Делаем так, чтобы все адреса из нашего приложения (simpleapp/urls.py)
    # подключались к главному приложению с префиксом products/.
